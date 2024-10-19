e9f9808a2d2f3b6c392af64f02166c49c3ce275b
Wieczorek Adam <awieczorek@bsb.com.pl>
2024-10-19 11:04:45 +0200

    init

---
+++ src/main/java/pl/demo/zwinne/ProgramowanieZwinneBeApplication.java
@@ -0,0 +1,13 @@
+package pl.demo.zwinne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramowanieZwinneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramowanieZwinneBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/demo/zwinne/ProgramowanieZwinneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.demo.zwinne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramowanieZwinneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
